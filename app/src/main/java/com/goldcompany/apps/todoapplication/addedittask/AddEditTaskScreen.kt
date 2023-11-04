53038a08fe70c42ee8f45eb6663f7cbd3228cc8f
JunghunKum <123260423+kumgold@users.noreply.github.com>
2023-11-04 16:57:42 +0900

    add date picker

--- app/src/main/java/com/goldcompany/apps/todoapplication/addedittask/AddEditTaskScreen.kt
+++ app/src/main/java/com/goldcompany/apps/todoapplication/addedittask/AddEditTaskScreen.kt
@@ -1,7 +1,9 @@
 package com.goldcompany.apps.todoapplication.addedittask
 
 import androidx.annotation.StringRes
-import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -10,32 +12,43 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.DatePicker
+import androidx.compose.material3.DatePickerDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SelectableDates
 import androidx.compose.material3.SnackbarDuration
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.goldcompany.apps.todoapplication.R
 import com.goldcompany.apps.todoapplication.compose.LoadingAnimation
 import com.goldcompany.apps.todoapplication.compose.TitleTopAppBar
+import java.time.Instant
+import java.time.LocalDateTime
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AddEditTaskScreen(
     modifier: Modifier = Modifier,
@@ -149,6 +162,20 @@ private fun EditTaskScreen(
             isSingleLine = true
         )
         Spacer(modifier = Modifier.height(dimensionResource(id = R.dimen.vertical_margin)))
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = dimensionResource(id = R.dimen.horizontal_margin))
+        ) {
+            SelectTaskDatesView(
+                modifier = Modifier.weight(1f)
+            )
+            Spacer(modifier = Modifier.width(dimensionResource(id = R.dimen.horizontal_margin)))
+            SelectTaskDatesView(
+                modifier = Modifier.weight(1f)
+            )
+        }
+        Spacer(modifier = Modifier.height(dimensionResource(id = R.dimen.vertical_margin)))
         TaskTextInputView(
             modifier = Modifier
                 .fillMaxWidth()
@@ -181,7 +208,95 @@ private fun EditTaskScreen(
     }
 }
 
+@Composable
+private fun SelectTaskDatesView(
+    modifier: Modifier,
+) {
+    var date by remember {
+        mutableStateOf("Show Date Picker")
+    }
+    var isShowDatePickerDialog by remember {
+        mutableStateOf(false)
+    }
+
+    Box(
+        modifier = modifier.border(
+            width = 1.dp,
+            color = colorResource(id = R.color.black),
+            shape = RoundedCornerShape(2.dp)
+        ),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            modifier = Modifier
+                .fillMaxWidth()
+                .clickable {
+                isShowDatePickerDialog = true
+            },
+            text = date,
+            textAlign = TextAlign.Center
+        )
+    }
+
+    if (isShowDatePickerDialog) {
+        TaskDatePickerDialog(
+            onDateSelected = { date = it },
+            onDismiss = { isShowDatePickerDialog = false }
+        )
+    }
+}
+
 @OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun TaskDatePickerDialog(
+    onDateSelected: (String) -> Unit,
+    onDismiss: () -> Unit
+) {
+    val datePickerState = rememberDatePickerState(
+        selectableDates = object : SelectableDates {
+            override fun isSelectableDate(utcTimeMillis: Long): Boolean {
+                return utcTimeMillis <= System.currentTimeMillis()
+            }
+        }
+    )
+    val selectedDate = datePickerState.selectedDateMillis?.let {
+        convertMilliToDate(it)
+    } ?: ""
+
+    DatePickerDialog(
+        onDismissRequest = { onDismiss() },
+        confirmButton = {
+            Button(
+                onClick = {
+                    onDateSelected(selectedDate)
+                    onDismiss()
+                }
+            ) {
+                Text(text = stringResource(id = R.string.ok))
+            }
+        },
+        dismissButton = {
+            Button(
+                onClick = {
+                    onDismiss()
+                }
+            ) {
+                Text(text = stringResource(id = R.string.cancel))
+            }
+        }
+    ) {
+        DatePicker(state = datePickerState)
+    }
+}
+
+private fun convertMilliToDate(milli: Long): String {
+    val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
+    val instant = Instant.ofEpochMilli(milli)
+    val date = LocalDateTime.ofInstant(instant, ZoneId.systemDefault())
+
+    return formatter.format(date)
+}
+
 @Composable
 private fun TaskTextInputView(
     modifier: Modifier,
