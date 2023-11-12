389cd081c84e1201f2a8e99b864d10104eb43b7b
JunghunKum <123260423+kumgold@users.noreply.github.com>
2023-11-12 22:42:06 +0900

    making widgets

--- app/src/main/java/com/goldcompany/apps/todoapplication/widget/TaskAppWidget.kt
+++ app/src/main/java/com/goldcompany/apps/todoapplication/widget/TaskAppWidget.kt
@@ -1,12 +1,48 @@
 package com.goldcompany.apps.todoapplication.widget
 
 import android.content.Context
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.unit.dp
 import androidx.glance.GlanceId
+import androidx.glance.GlanceModifier
+import androidx.glance.LocalSize
+import androidx.glance.appwidget.CheckBox
 import androidx.glance.appwidget.GlanceAppWidget
+import androidx.glance.appwidget.GlanceAppWidgetReceiver
+import androidx.glance.appwidget.SizeMode
+import androidx.glance.appwidget.appWidgetBackground
+import androidx.glance.appwidget.cornerRadius
 import androidx.glance.appwidget.provideContent
+import androidx.glance.background
+import androidx.glance.layout.Alignment
+import androidx.glance.layout.Row
+import androidx.glance.layout.fillMaxSize
+import androidx.glance.layout.padding
+import com.goldcompany.apps.todoapplication.R
 
 class TaskAppWidget : GlanceAppWidget() {
+
+    companion object {
+        private val SMALL_SQUARE = DpSize(100.dp, 100.dp)
+        private val HORIZONTAL_RECTANGLE = DpSize(250.dp, 100.dp)
+        private val BIG_SQUARE = DpSize(250.dp, 250.dp)
+    }
+
+    override val sizeMode = SizeMode.Responsive(
+        setOf(
+            SMALL_SQUARE,
+            HORIZONTAL_RECTANGLE,
+            BIG_SQUARE
+        )
+    )
+
     override suspend fun provideGlance(context: Context, id: GlanceId) {
         provideContent {
             TaskContents()
@@ -14,9 +50,34 @@ class TaskAppWidget : GlanceAppWidget() {
     }
 
     @Composable
-    private fun TaskContents(
+    private fun TaskContents() {
+        var isChecked by remember {
+            mutableStateOf(false)
+        }
+        val size = LocalSize.current
 
-    ) {
-
+        Row(
+            modifier = GlanceModifier.fillMaxSize()
+                .background(Color.White)
+                .padding(R.dimen.default_margin)
+                .cornerRadius(R.dimen.default_corner_radius)
+                .appWidgetBackground(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            CheckBox(
+                checked = isChecked,
+                onCheckedChange = {
+                    isChecked = !isChecked
+                }
+            )
+            if (size == HORIZONTAL_RECTANGLE) {
+                Text(text = "test")
+            }
+        }
     }
+}
+
+class TaskAppWidgetReceiver : GlanceAppWidgetReceiver() {
+    override val glanceAppWidget: GlanceAppWidget = TaskAppWidget()
 }
--- app/src/main/java/com/goldcompany/apps/todoapplication/widget/TaskAppWidgetReceiver.kt
+++
@@ -1,8 +0,0 @@
-package com.goldcompany.apps.todoapplication.widget
-
-import androidx.glance.appwidget.GlanceAppWidget
-import androidx.glance.appwidget.GlanceAppWidgetReceiver
-
-class TaskAppWidgetReceiver : GlanceAppWidgetReceiver() {
-    override val glanceAppWidget: GlanceAppWidget = TaskAppWidget()
-}
