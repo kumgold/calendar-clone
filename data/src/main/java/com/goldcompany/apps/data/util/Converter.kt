740db83ebb1b9f14bfe8f147a99011d38abc13e6
JunghunKum <123260423+kumgold@users.noreply.github.com>
2023-12-09 14:46:30 -0800

    Update Converter.kt

--- data/src/main/java/com/goldcompany/apps/data/util/Converter.kt
+++ data/src/main/java/com/goldcompany/apps/data/util/Converter.kt
@@ -7,13 +7,16 @@ import java.time.LocalTime
 import java.time.ZoneId
 import java.time.ZoneOffset
 import java.time.format.DateTimeFormatter
+import java.util.TimeZone
+
+private const val DATE_PATTERN = "yyyy-MM-dd"
 
 fun convertMilliToDate(milli: Long?): String? {
     if (milli == null) return null
 
-    val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
+    val formatter = DateTimeFormatter.ofPattern(DATE_PATTERN)
     val instant = Instant.ofEpochMilli(milli)
-    val date = LocalDateTime.ofInstant(instant, ZoneId.systemDefault())
+    val date = LocalDateTime.ofInstant(instant, TimeZone.getTimeZone("UTC").toZoneId())
 
     return formatter.format(date)
 }
@@ -21,7 +24,7 @@ fun convertMilliToDate(milli: Long?): String? {
 fun convertDateToMilli(date: String?): Long? {
     if (date == null) return null
 
-    val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
+    val formatter = DateTimeFormatter.ofPattern(DATE_PATTERN)
     val localDate = LocalDate.parse(date, formatter)
 
     return LocalDateTime.of(localDate, LocalTime.of(0, 0))
