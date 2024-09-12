fc79adc90e95e4e61255bc97627ec18011c88647
JunghunKum <123260423+kumgold@users.noreply.github.com>
2024-09-12 19:36:05 +0900

    rename

--- app/src/main/java/com/goldcompany/apps/todoapplication/CalendarApplication.kt
+++ app/src/main/java/com/goldcompany/apps/todoapplication/CalendarApplication.kt
@@ -4,8 +4,4 @@ import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class TodoApplication : Application() {
-    override fun onCreate() {
-        super.onCreate()
-    }
-}
+class CalendarApplication : Application()
